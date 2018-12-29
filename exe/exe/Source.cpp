80d8b7d99b094d656587ee2995bed19769fe2e14
Pasha831 <46136468+Pasha831@users.noreply.github.com>
2018-12-29 18:55:09 +0300

    Update Source.cpp

--- exe/exe/Source.cpp
+++ exe/exe/Source.cpp
@@ -8,11 +8,11 @@ struct Link
 	Link* left;
 	Link* right;
 };
-
 class Tree
 {
 private:
 	Link* root;
+	void addin(Link* tmp, int d);
 public:
 	Tree()
 	{
@@ -23,7 +23,40 @@ public:
 		delete root;
 	}
 	void add(int d);
+	void show();
 };
+
+void Tree::addin(Link* tmp, int d)
+{
+	if (d > tmp->data) // IF d > root->data 
+	{
+		if (tmp->right == NULL)
+		{
+			tmp->right = new Link;
+			tmp->right->data = d;
+			tmp->right->right = NULL;
+			tmp->right->left = NULL;
+		}
+		else
+		{
+			addin(tmp->right, d);
+		}
+	}
+	else // IF d <= root->data
+	{
+		if (tmp->left == NULL)
+		{
+			tmp->left = new Link;
+			tmp->left->data = d;
+			tmp->left->left = NULL;
+			tmp->left->right = NULL;
+		}
+		else
+		{
+			addin(tmp->left, d);
+		}
+	}
+}
 void Tree::add(int d)
 {
 	if (root == NULL)
@@ -35,32 +68,27 @@ void Tree::add(int d)
 	}
 	else
 	{
-		Link* tmp = root;
-		bool t = false;
-		while (!t)
-		{
-			if (d > tmp->data)
-			{
-				tmp->right = new Link;
-				tmp->right->data = d;
-			}
-			if (d <= tmp->data)
-			{
-				tmp->left = new Link;
-				tmp->left->data = d;
-			}
-			else
-			{
-
-			}
-			t = true;
-		}
+		addin(root, d);
 	}
+}
+void Tree::show() // Left - Root - Right
+{
+	
 }
 int main()
 {
 	Tree tree;
+
+	tree.add(4);
+	tree.add(21);
+	tree.add(313);
+	tree.add(19);
+
+	tree.add(2);
 	tree.add(0);
-	tree.add(6);
+	tree.add(3);
+
+	tree.show();
+
 	system("pause");
 }
