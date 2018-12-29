f70f73b82002e497bfae772e8e1db84d6f6af2ce
Pasha831 <46136468+Pasha831@users.noreply.github.com>
2018-12-29 16:12:58 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+struct Link
+{
+	int data;
+
+	Link* left;
+	Link* right;
+};
+
+class Tree
+{
+private:
+	Link* root;
+public:
+	Tree()
+	{
+		root = NULL;
+	}
+	~Tree()
+	{
+		delete root;
+	}
+	void add(int d);
+	void addin(Link* tmp, int d);
+};
+void Tree::addin(Link* tmp, int d)
+{
+	if (d > tmp->data) // IF d > root->data 
+	{
+		if (tmp->right == NULL)
+		{
+			tmp->right = new Link;
+			tmp->right->data = d;
+			tmp->right->right = NULL;
+			tmp->right->left = NULL;
+		}
+		else
+		{
+			addin(tmp->right, d);
+		}
+	}
+	else // IF d <= root->data
+	{
+		if (tmp->left == NULL)
+		{
+			tmp->left = new Link;
+			tmp->left->data = d;
+			tmp->left->left = NULL;
+			tmp->right->right = NULL;
+		}
+		else
+		{
+			addin(tmp->left, d);
+		}
+	}
+}
+void Tree::add(int d)
+{
+	if (root == NULL)
+	{
+		root = new Link;
+		root->data = d;
+		root->left = NULL;
+		root->right = NULL;
+	}
+	else
+	{
+		addin(root, d);
+	}
+}
+int main()
+{
+	Tree tree;
+	tree.add(4);
+	tree.add(21);
+	tree.add(313);
+	tree.add(2);
+	tree.add(1);
+	//tree.add(0);
+	system("pause");
+}
